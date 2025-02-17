c4bc35db44f789cd993f3213749112e1645ef353
ElectricalThinking29 <140239795+ElectricalThinking29@users.noreply.github.com>
2025-02-17 22:26:26 +0900

    Add files via upload
    Replacing 2 old files

---
+++ Code/src/Receiver_v0-3_mini.cpp
@@ -0,0 +1,110 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+// /*** MY CHANNEL ***
+//	If you can, please support me on my Youtube's channel:
+//
+//		www.youtube.com/@TheElectricTHINK
+//
+//	by hitting like, share and subscribe's buttons to my videos.
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// /*** LICENSE ***
+//
+// Copyright 2025 @ElectricalThinking29
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file		Receiver_V0.cpp
+ * @author		The ElectricTHINK
+ * @brief 		ESP-NOW Receiver
+ * @version 	0.3
+ * @remarks		Program for esp32 receiving data from sender using ESP-NOW.
+ * 				It will immediately print out the sender MAC address and received data
+ * 				whenever it receives a full message.
+ * 				The printed result will be in Hex, String, Unsigned Long Long Int, and Signed Long Long Int.
+*/
+
+#include "Arduino.h"
+#include "WiFi.h"
+// #include "esp_wifi.h"
+#include "esp_now.h"
+
+
+#define U8_MESSAGE_SIZE 		(uint8_t)8				// Received message [Size](Bytes)
+#define U8_MAC_ADDR_SIZE		(uint8_t)6				// Sender MAC Address [Size](Bytes)
+
+
+bool 		b_message_received;													// Flag for message received
+uint8_t 	u8_message_data[U8_MESSAGE_SIZE];									// Received message
+uint8_t		u8_Sender_MAC[U8_MAC_ADDR_SIZE];									// Sender MAC address
+
+
+void OnDataRecv(const uint8_t *pu8_mac, const uint8_t *pu8_incomingData, int i32_len);
+
+
+void setup()
+{
+	b_message_received = false;													// Set receive flag to false
+
+	Serial.begin(115200);														// Initialize Serial port for debug
+	
+	Serial.println("Init ESP-NOW Receiver\n");									// Init ESP-NOW
+	
+	WiFi.mode(WIFI_STA);														// Set Wifi mode to STA mode
+	esp_now_init();
+	esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));					// Register ESP-NOW receive callback function
+
+	Serial.println("ESP-NOW Receiver initialized\n");
+}
+ 
+void loop()
+{
+	if (b_message_received)
+	{
+		b_message_received = false;
+
+		Serial.println("\nReceived message from sender");						// Print Sender MAC
+		Serial.print("Sender MAC: ");
+		for (int i = 0; i < U8_MAC_ADDR_SIZE - 1; i++)
+		{
+			Serial.print(u8_Sender_MAC[i], HEX);
+			Serial.print(":");
+		}
+		Serial.println(u8_Sender_MAC[5], HEX);
+
+		Serial.printf("Received Message:  %s\n", u8_message_data);				// Print message
+	}
+}
+ 
+/// @brief Callback function when ESP-NOW received data
+void OnDataRecv(const uint8_t *pu8_mac, const uint8_t *pu8_incomingData, int i32_len)
+{
+	if (i32_len == U8_MESSAGE_SIZE)
+	{
+		b_message_received = true;
+
+		for (int i = 0; i < U8_MAC_ADDR_SIZE; i++)
+		{
+			u8_Sender_MAC[i] = pu8_mac[i];
+		}
+		
+		for (int i = 0; i < U8_MESSAGE_SIZE ; i++)
+		{
+			u8_message_data[i] = pu8_incomingData[i];
+		}
+	}
+}
---
+++ Code/src/Sender_v1-1_mini.cpp
@@ -0,0 +1,135 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+// /*** MY CHANNEL ***
+//	If you can, please support me on my Youtube's channel:
+//
+//		www.youtube.com/@TheElectricTHINK
+//
+//	by hitting like, share and subscribe's buttons to my videos.
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// /*** LICENSE ***
+//
+// Copyright 2025 @ElectricalThinking29
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file		Sender_V03.cpp
+ * @author		The ElectricTHINK
+ * @brief 		ESP-NOW Sender
+ * @version 	1.1
+ * @remarks		- Program for esp32 sending data to other receiver using ESP-NOW for every 1 second.
+ * 				- This program use no delay() function. So it will not block the loop.
+ * 				- Depending on sent result:
+ * 					If sent successfully, it will toggle LED in 500 ms.
+ * 					If sent failed, it will turn off LED.
+*/
+
+#include "Arduino.h"
+#include "WiFi.h"
+// #include "esp_wifi.h"
+#include "esp_now.h"
+
+
+#define U8_MESSAGE_SIZE 		(uint8_t)8								// Message size in Bytes
+#define U8_MAC_ADDR_SIZE		(uint8_t)6								// MAC Address size in Bytes
+#define U32_T500MS				(uint32_t)500							// 500 Milliseconds
+#define U32_T1S					(uint32_t)1000							// 1000 Milliseconds (= 1 second)
+
+
+/// @brief MAC address of ESP-NOW Receiver information
+const esp_now_peer_info_t st_ESPNOW_Receiver_info = {
+    .peer_addr	= {	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},				// MAC address for Broadcasting. Change this to your receiver's MAC address
+    .lmk		= {	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+                    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    .channel	= 0,
+    .ifidx		= WIFI_IF_STA,
+    .encrypt	= false,
+    .priv		= 0x00000000,											// 32-bit OS pointer
+};
+
+
+bool b_is_LED_HIGH;														// Store status of LED
+
+
+void ToggleLED(void);
+void TurnOffLED(void);
+
+
+void setup()
+{
+    Serial.begin(115200);												// Initialize Serial port for debug
+    pinMode(BUILTIN_LED, OUTPUT);										// Initialize LED
+
+    Serial.println("ESP-NOW Sender Initialize...");
+
+    WiFi.mode(WIFI_MODE_STA);											// Set WiFi mode to STA mode
+    esp_now_init();														// Init ESP-NOW
+    esp_now_add_peer(&st_ESPNOW_Receiver_info);							// Setting peer info
+
+    Serial.println("ESP-NOW Sender Initialized.");
+}
+
+
+void loop()
+{
+    uint8_t pu8_mes[U8_MESSAGE_SIZE] = "ESP-NOW";						// The sent data is 8 bytes. Make sure that the received data variable is also set to 8 bytes.
+    static uint32_t u32_previous_T1sec_time 	= 0;					// The previous milis value
+    static uint32_t u32_previous_T500ms_time	= 0;					// The previous milis value
+    static esp_err_t i32_status = ESP_FAIL;								// This is for debugging
+
+    // 500 ms cycle
+    if (millis() - u32_previous_T500ms_time >= U32_T500MS)
+    {
+        u32_previous_T500ms_time += U32_T500MS;
+		
+        if (i32_status == ESP_OK)
+        {
+            ToggleLED();
+        }
+        else
+        {
+            TurnOffLED();
+        }
+    }
+    
+    // 1 second cycle
+    if (millis() - u32_previous_T1sec_time >= U32_T1S)
+    {
+        u32_previous_T1sec_time += U32_T1S;
+
+		// Send message via ESP-NOW
+        i32_status = esp_now_send(st_ESPNOW_Receiver_info.peer_addr, pu8_mes, U8_MESSAGE_SIZE);
+    }
+}
+
+
+/// @brief  Toogle LED. Oon ESP32 S2 Mini board it is GPIO 15
+/// @param  void
+void ToggleLED(void)
+{
+    b_is_LED_HIGH = !b_is_LED_HIGH;
+    digitalWrite(BUILTIN_LED, b_is_LED_HIGH);
+}
+
+/// @brief  Turn off LED. On ESP32 S2 Mini board it is GPIO 15
+/// @param  void
+void TurnOffLED(void)
+{
+    b_is_LED_HIGH = false;
+    digitalWrite(BUILTIN_LED, LOW);
+}
